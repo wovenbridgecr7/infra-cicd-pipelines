ca0982e1d036a06ebe8918af5546d0e16e383aa1
wovenbridgecr7 <kashishupadhyay00@gmail.com>
2024-08-11 15:04:00 +0530

    testchange2

--- wholeCodePipelineDevops/provider.tf
+++ wholeCodePipelineDevops/provider.tf
@@ -5,14 +5,14 @@ terraform {
       version = ">=1.0.0"
     }
   }
-
+/*
   backend "azurerm" {
     resource_group_name  = "my-tes-rg-tf-state"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
     storage_account_name = "testsafortfstatestorewov"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "tfstate"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "vm.prod.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
-
+*/
 
 
 }
