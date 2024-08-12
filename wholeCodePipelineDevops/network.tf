9ece39bf1cb34f302d883dc4f6801bf48fa26149
wovenbridgecr7 <kashishupadhyay00@gmail.com>
2024-08-12 16:28:02 +0530

    testingnsgrules

--- wholeCodePipelineDevops/network.tf
+++ wholeCodePipelineDevops/network.tf
@@ -34,7 +34,7 @@ resource "azurerm_virtual_network" "my-test-prod-vnet" {
      private_ip_address_allocation = "Dynamic"
      #private_ip_address = "172.168.1.2"
      public_ip_address_id = azurerm_public_ip.my-test-ip.id
-
+     
    } 
    
    }
@@ -44,7 +44,7 @@ resource "azurerm_virtual_network" "my-test-prod-vnet" {
     name = var.sg_name
     resource_group_name = azurerm_resource_group.my-test-prod-rg-01.name
     location = azurerm_resource_group.my-test-prod-rg-01.location
-
+    
  }
 
   resource "azurerm_network_security_rule" "my-test-sgrule" {
@@ -60,3 +60,22 @@ resource "azurerm_virtual_network" "my-test-prod-vnet" {
         resource_group_name = azurerm_resource_group.my-test-prod-rg-01.name
         network_security_group_name = azurerm_network_security_group.my-test-sg.name
   }
+
+  resource "azurerm_network_security_rule" "my-test-sgrule" {
+        name                       = "test456"
+        priority                   = 110
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "8080"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+        resource_group_name = azurerm_resource_group.my-test-prod-rg-01.name
+        network_security_group_name = azurerm_network_security_group.my-test-sg.name
+  }
+
+  resource "azurerm_network_interface_security_group_association" "associate_networknsg" {
+  network_interface_id      = azurerm_network_interface.my-test-interf
+  network_security_group_id = azurerm_network_security_group.my-test-sg
+}
